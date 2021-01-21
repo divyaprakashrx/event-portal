0df55194232ffff3861298a847bf0679a29a140e
Divya Prakash <medivyaprakash@gmail.com>
2021-01-21 23:12:38 +0530

    Material UI add

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,14 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="/public/icons/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
         </Head>
         <body>
           <Main />
